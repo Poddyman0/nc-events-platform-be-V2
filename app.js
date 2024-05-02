9b57342ec64ee2bf370c643a87614e46fe2101ef
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-05-02 14:36:49 +0100

    CSP fix

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' *");
 
   next();
 })
