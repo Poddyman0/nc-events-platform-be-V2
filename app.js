5e5f45a5c64d4e73ca39b839b80b77caef211df9
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-05-02 08:25:32 +0100

    CSP fix

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src 'self' http://nc-events-platform-be-v2-production.up.railway.app");
+
   next();
 })
 
